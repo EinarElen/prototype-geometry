cb510181d7c2ff294b9c316d8e469bdb9b0d1db6
Einar Elén <einar.elen@gmail.com>
2022-06-13 16:55:16 +0200

    Updated constants from GDML

--- code/prototype-geometry-reference.py
+++ code/prototype-geometry-reference.py
@@ -10,43 +10,113 @@ import numpy as np
 center = np.array([0., 0., 0.])
 hadron_calorimeter_pos = np.array([0., 0., 0.])
 world_dim = 10000.
-air_thickness = 2.
-absorber_width = 800.
-absorber_ears = 50.
-absorber_thickness = 20
-bar_mounting_plate_thickness = 3
+absorber_width = 665.
+absorber_height = 700.
+absorber_thickness = 20.
+absorber_hole_radius = 6.5
+absorber_hole_distance_from_edge = 16.
+bar_mounting_plate_thickness = 3.
 scint_thickness = 20.
 scint_bar_length = 2000.
-layer_thickness = absorber_thickness + scint_thickness + \
-    bar_mounting_plate_thickness + air_thickness
+scint_bar_width = 50.
+scint_bar_cover_thickness = 0.5
+
+layer_thickness = 45.  # Note hardcoded
+air_thickness = layer_thickness - (
+    absorber_thickness +
+    bar_mounting_plate_thickness +
+    scint_thickness +
+    scint_bar_cover_thickness)  # Derived from other variables now
+
+num_bars_front = 8
+num_bars_back = 12
 num_layers_front_vertical = 4
 num_layers_front_horizontal = 5
-num_layers_front=num_layers_front_vertical + num_layers_front_horizontal
+num_layers_front = num_layers_front_vertical + num_layers_front_horizontal
 num_layers_back_vertical = 5
 num_layers_back_horizontal = 5
 num_layers_back = num_layers_back_vertical + num_layers_back_horizontal
 num_layers = num_layers_front + num_layers_back
+num_absorbers = num_layers + 1  # 1 Extra plate in the back
 back_start = num_layers_front * layer_thickness
-scint_bar_width = 50.
-num_bars_front = 8
-num_bars_back = 12
-scint_front_vertical_x = num_bars_front * scint_bar_width
-scint_front_vertical_y = scint_bar_length
-scint_front_horizontal_x = scint_bar_length
-scint_front_horizontal_y = num_bars_front * scint_bar_width
-scint_back_vertical_x = num_bars_back * scint_bar_width
-scint_back_vertical_y = scint_bar_length
-scint_back_horizontal_x = scint_bar_length
-scint_back_horizontal_y = num_bars_back * scint_bar_width
+
 dx = 3000.
 dy = 3000.
 dz = num_layers * layer_thickness
+
+first_layer_zpos = -dz / 2
+first_layer_absorber_zpos = first_layer_zpos + absorber_thickness / 2
+first_layer_scint_cover_zpos = first_layer_zpos + \
+    absorber_thickness + \
+    scint_bar_cover_thickness / 2
+assert (first_layer_scint_cover_zpos ==
+        first_layer_absorber_zpos +
+        absorber_thickness / 2 +
+        scint_bar_cover_thickness / 2)
+first_layer_scint_zpos = first_layer_zpos + \
+    absorber_thickness + \
+    scint_bar_cover_thickness + \
+    scint_thickness / 2
+assert (first_layer_scint_zpos ==
+        first_layer_scint_cover_zpos +
+        scint_bar_cover_thickness / 2 +
+        scint_thickness / 2)
+first_layer_bar_mounting_plate_zpos = first_layer_zpos + \
+    absorber_thickness + \
+    scint_bar_cover_thickness + \
+    scint_thickness + \
+    bar_mounting_plate_thickness / 2
+assert (first_layer_bar_mounting_plate_zpos ==
+        first_layer_scint_zpos +
+        scint_thickness / 2 +
+        bar_mounting_plate_thickness / 2)
+
+first_back_layer_zpos = first_layer_zpos + back_start
+
+absorber_first_layer_zpos = first_layer_zpos
+
+scint_front_vertical_x = num_bars_front * scint_bar_width
+scint_front_vertical_y = scint_bar_length
+
+scint_front_horizontal_x = scint_bar_length
+scint_front_horizontal_y = num_bars_front * scint_bar_width
+
+scint_back_vertical_x = num_bars_back * scint_bar_width
+scint_back_vertical_y = scint_bar_length
+
+scint_back_horizontal_x = scint_bar_length
+scint_back_horizontal_y = num_bars_back * scint_bar_width
+
+
+bar_mounting_plate_horizontal_x = scint_back_horizontal_x
+bar_mounting_plate_horizontal_y = scint_back_horizontal_y
+bar_mounting_plate_vertical_x = scint_back_vertical_x
+bar_mounting_plate_vertical_y = scint_back_vertical_y
+
 trigger_bar_gap = 0.3
-trigger_bar_dx = 40
-trigger_bar_dy = 3
-trigger_bar_dz = 2
-number_of_bars = 6
-trigger_layer_distance_from_detector = 300
+trigger_bar_dx = 40.
+trigger_bar_dy = 3.
+trigger_bar_dz = 2.
+number_of_trigger_bars = 6
+trigger_layer_distance_from_detector = 300.
+
+lyso_bar_gap = 0.3
+lyso_bar_dx = 30
+lyso_bar_dy = 2.1
+lyso_bar_dz = 0.6
+number_of_lyso_bars = 8
+
+lyso_plate_dx = 30
+lyso_plate_dy = 20
+lyso_plate_dz = 1.2
+
+
+prototype_extra_space = 1000.
+trigger_dz = trigger_layer_distance_from_detector + \
+    trigger_bar_dz + \
+    trigger_bar_gap
+
+prototype_box_dz = dz + 2 * trigger_dz + prototype_extra_space
 
 # Solids
 front_vertical_scint_box = {"width": scint_front_vertical_x,
@@ -75,16 +145,18 @@ trigger_bar_box = {"width": trigger_bar_dx,
 class physical_volume:
     def __init__(self, position, name="", CopyNumber=None):
         self.position = position
-        self.name = name
         if CopyNumber is not None:
             self.CopyNumber = CopyNumber
-        self.depth = position[2]
+            self.depth = position[2]
+
     def __repr__(self):
         return str(self.__dict__)
 
 
 # Physical volumes
-absorber_first_layer_pos = np.array([0, 0, -dz/2])
+absorber_first_layer_pos = np.array([0.,
+                                     0.,
+                                     -dz/2])
 bar_mounting_plate_horizontal_first_layer_pos = np.array(
     [0, 0,
      absorber_first_layer_pos[2] +
